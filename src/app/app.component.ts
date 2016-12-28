bbf7b23102466c9d4c429d793b0780c70355e50e
actimeller <max.post@bk.ru>
2016-12-28 23:21:50 +0300

    regForm as reactForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,36 @@ export class AppComponent {
   title = 'app works!';
 
 
-  loginSubmit(val){
+  loginSubmit(val) {
+    alert(JSON.stringify(val));
+  }
+
+
+
+  public reactForm: FormGroup;
+
+  public constructor(private _fb: FormBuilder) {
+    this.reactForm = this._fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(5)]],
+      lastname: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [this.passwordValidator]],
+      password: ['', [this.passwordValidator, Validators.required, Validators.minLength(5)]]
+    })
+  }
+
+  public emailValidator(control:FormControl):{[key:string]:boolean}{
+    const value = control.value || '';
+    const valid = value.match(/\S+@\S+\.\S+/);
+    return valid ? null : {email:true}
+  }
+
+  public passwordValidator(control:FormControl):{[key:string]:boolean}{
+    const value = control.value || '';
+    const valid = value.match(/^\d{9}$/);
+    return valid ? null : {password:true}
+  }
+
+  regSubmit(val) {
     alert(JSON.stringify(val));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
