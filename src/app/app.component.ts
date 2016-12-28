e3fe7d4d6017c0ea1be6736a821238d79057f5b2
actimeller <max.post@bk.ru>
2016-12-29 01:06:41 +0300

    app works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {Component, ElementRef } from '@angular/core';
+import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,36 +10,47 @@ export class AppComponent {
   title = 'app works!';
 
 
-  loginSubmit(val) {
+  public loginSubmit(val) {
     alert(JSON.stringify(val));
-  }
+  };
 
-
-
-  public reactForm: FormGroup;
+  public reactiveForm: FormGroup;
 
   public constructor(private _fb: FormBuilder) {
-    this.reactForm = this._fb.group({
-      firstname: ['', [Validators.required, Validators.minLength(5)]],
+    this.reactiveForm = this._fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(5), this.myFunc]],
       lastname: ['', [Validators.required, Validators.minLength(5)]],
-      email: ['', [this.passwordValidator]],
+      email: ['', [ Validators.required, this.emailValidator]],
       password: ['', [this.passwordValidator, Validators.required, Validators.minLength(5)]]
-    })
+    });
+
+    this.reactiveForm.valueChanges.subscribe(data => this.onValueChanged(data));
   }
 
-  public emailValidator(control:FormControl):{[key:string]:boolean}{
+  public onValueChanged(data) {
+    console.info(data);
+  }
+
+  public myFunc(control: FormControl): {[key: string]: boolean} {
     const value = control.value || '';
-    const valid = value.match(/\S+@\S+\.\S+/);
-    return valid ? null : {email:true}
+    return value.length > 0 ? null : {hideLabel: true}
+    // как обрабатывать ошибки? Вывести свой класс или что-то вроде этого
   }
 
-  public passwordValidator(control:FormControl):{[key:string]:boolean}{
+  public emailValidator(control: FormControl): {[key: string]: boolean} {
+    const value = control.value || '';
+    const valid = value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    return valid ? null : {email: true}
+  }
+
+  public passwordValidator(control: FormControl): {[key: string]: boolean} {
     const value = control.value || '';
     const valid = value.match(/^\d{9}$/);
-    return valid ? null : {password:true}
+    return valid ? null : {password: true}
   }
 
-  regSubmit(val) {
+  public regSubmit(val) {
     alert(JSON.stringify(val));
   }
 }
